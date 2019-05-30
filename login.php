0fef3db7880083d3d3c67ff91ace70cb1a6a7afe
WillHalloward <willhalloward@gmail.com>
2019-05-30 14:00:26 +0200

    Fixed Meny and menu icon not being connected, which made Meny float behind on larger screens.
    Signed-off-by: WillHalloward <willhalloward@gmail.com>

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,32 +7,17 @@
 <body>
 <header>
 
-<h1>This is a heading</h1>
-<p>This is a paragraph.</p>
-<div>
-<?php
-    if(isset($_SESSION['anvandarid']))
-    {
-        echo '<form action="includes/logout.inc.php" method="Post"> <!--skickar data till fil logout.inc.php i mappen includes-->
-        <button type="submit" name="logout-submit">Logga ut</button>
-        </form> ';
-    }
-    else
-    {
-        echo '<form action="includes/login.inc.php" method="Post"> <!--skickar data till fil login.inc.php i mappen includes-->
-        <input type="text" name="anvandarnamn" placeholder = "Anvandarnamn"> 
-        <input type="password" name="losenord" placeholder = "losenord">
-        <button type="submit" name="login-submit">Logga in</button>
-        </form> <a href="skapa_användare.php">Skapa anvandare</a> ';
-    }
-?>
-
-
-
-
-
-
-</div>
+    <h1>This is a heading</h1>
+    <p>This is a paragraph.</p>
+    <div>
+        <?php
+        if (isset($_SESSION['anvandarid'])) {
+            echo '<form action="includes/logout.inc.php" method="Post"> <button type="submit" name="logout-submit">Logga ut</button> </form> ';
+        } else {
+            echo '<form action="includes/login.inc.php" method="Post"> <input type="text" name="anvandarnamn" placeholder = "Anvandarnamn"> <input type="password" name="losenord" placeholder = "losenord"> <button type="submit" name="login-submit">Logga in</button> </form> <a href="skapa_användare.php">Skapa anvandare</a> ';
+        }
+        ?>
+    </div>
 </header>
 </body>
 </html>
