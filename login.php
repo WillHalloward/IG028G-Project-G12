7c0d79f5080ee9baae2df2a2328038ff1129fdb9
WillHalloward <willhalloward@gmail.com>
2019-05-30 14:40:56 +0200

    Made Genre.html and Index.html "glömt lösenord" have the same margin-right as other pages.
    Signed-off-by: WillHalloward <willhalloward@gmail.com>

--- login.php
+++ login.php
@@ -1,23 +1,10 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<title>HTML Tutorial</title>
-<body>
-<header>
-
-    <h1>This is a heading</h1>
-    <p>This is a paragraph.</p>
-    <div>
-        <?php
-        if (isset($_SESSION['anvandarid'])) {
-            echo '<form action="includes/logout.inc.php" method="Post"> <button type="submit" name="logout-submit">Logga ut</button> </form> ';
-        } else {
-            echo '<form action="includes/login.inc.php" method="Post"> <input type="text" name="anvandarnamn" placeholder = "Anvandarnamn"> <input type="password" name="losenord" placeholder = "losenord"> <button type="submit" name="login-submit">Logga in</button> </form> <a href="skapa_användare.php">Skapa anvandare</a> ';
-        }
-        ?>
-    </div>
-</header>
-</body>
-</html>
+<?php session_start(); ?>
+<div>
+    <?php
+    if (isset($_SESSION['anvandarid'])) {
+        echo '<form action="includes/logout.inc.php" method="Post"> <button type="submit" name="logout-submit">Logga ut</button> </form> ';
+    } else {
+        echo '<form action="includes/login.inc.php" method="Post"> <input type="text" name="anvandarnamn" placeholder = "Anvandarnamn"> <input type="password" name="losenord" placeholder = "losenord"> <button type="submit" name="login-submit">Logga in</button> </form> <a href="skapa_användare.php">Skapa anvandare</a> ';
+    }
+    ?>
+</div>
